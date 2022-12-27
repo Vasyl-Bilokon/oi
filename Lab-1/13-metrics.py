76dfae148a376524263f4358deafe455a2df5b55
Vasyl-Bilokon <120991094+Vasyl-Bilokon@users.noreply.github.com>
2022-12-28 00:01:14 +0200

    Update and rename metrics.py to 13-metrics.py

--- Lab-1/13-metrics.py
+++ Lab-1/13-metrics.py
@@ -3,16 +3,21 @@ import numpy as np
 
 tf.disable_v2_behavior()
 
+# Placeholders
 x = tf.placeholder(tf.int32, [5])
 y = tf.placeholder(tf.int32, [5])
 
+# Metrics
 acc, acc_op = tf.metrics.accuracy(labels=x, predictions=y)
 
+# Session
 sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 sess.run(tf.local_variables_initializer())
 
+# Variable
 val = sess.run([acc, acc_op], feed_dict={x: [1, 1, 0, 1, 0], y: [0, 1, 0, 0, 1]})
 
+# Display results
 val_acc = sess.run(acc)
 print(val_acc)
