7a7b14598d65f3b4185860ff05d6fb5b2784f9ba
Vasyl-Bilokon <120991094+Vasyl-Bilokon@users.noreply.github.com>
2022-12-27 23:59:09 +0200

    Update and rename optimizers.py to 12-optimizers.py

--- Lab-1/12-optimizers.py
+++ Lab-1/12-optimizers.py
@@ -2,15 +2,19 @@ import tensorflow.compat.v1 as tf
 
 tf.disable_v2_behavior()
 
+# Variables definition
 x = tf.Variable(3, name='x', dtype=tf.float32)
 log_x = tf.log(x)
 log_x_squared = tf.square(log_x)
 
+# Optimizer
 optimizer = tf.train.GradientDescentOptimizer(0.7)
 train = optimizer.minimize(log_x_squared)
 
+# Initialization
 init = tf.global_variables_initializer()
 
+# Computing
 with tf.Session() as session:
     session.run(init)
     print("starting at", "x: ", session.run(x), "log(x)^2: ", session.run(log_x_squared))
